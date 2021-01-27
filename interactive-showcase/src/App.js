5cd01c0eb880307bc13e2bc6b746c8fc7d244d89
paublan <margot.paublan.etu@univ-lille.fr>
2021-01-27 17:28:09 +0100

    [Feature] Navigate between different routes
    Navigation enabled between the 2 routes ('/' and '/showcase-editor') : a button in the '/' route displays a connexion form ; is the user authentificates, he can pass through the '/showcase-editor' route, else it displays an alert.
    By now, contact to the API to authtificate the user doesn't work, all users can change routes

--- interactive-showcase/src/App.js
+++ interactive-showcase/src/App.js
@@ -1,6 +1,4 @@
-import {HashRouter,Link,Route,Switch} from "react-router-dom";
-
-
+import {HashRouter, Route, Switch} from "react-router-dom";
 import ShowcaseView from './components/routes/ShowcaseView.js'
 import ShowcaseEditor from './components/routes/ShowcaseEditor.js'
 
@@ -11,13 +9,9 @@ const App = (props)=> {
   return (
     <HashRouter>
     <div className="App">
-      <div className="menu">
-        <Link to="/"><h2>ShowcaseView</h2></Link>
-        <Link to="/showcase-editor"><h2>ShowcaseEditor</h2></Link>
-      </div>
       <Switch>
-        <Route exact path="/" component={ShowcaseView}/>
         <Route exact path="/showcase-editor" component={ShowcaseEditor}/>
+        <Route exact path="/" component={ShowcaseView}/>
       </Switch>     
     </div>
     </HashRouter>    
--- interactive-showcase/src/components/routes/ShowcaseEditor.js
+++ interactive-showcase/src/components/routes/ShowcaseEditor.js
@@ -1,8 +1,16 @@
 
-function ShowcaseEditor() {
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Gear } from 'react-bootstrap-icons';
+
+import {Link} from "react-router-dom";
+
+
+const ShowcaseEditor = () => {
   return (
     <div className="ShowcaseEditor">
-      <h1>Showcase editor</h1>
+        <Link to="/"><Button variant="light"><Gear/></Button></Link>
+        <h1>Showcase editor</h1>
     </div>
   );
 }
